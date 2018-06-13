b73bdec730811bf2e4893fe8a73115da81640b70
Zhang <zhangzhq28@mail2.sysu.edu.cn>
2018-06-13 11:46:42 +0800

    Update account.cpp

--- main/mk/account.cpp
+++ main/mk/account.cpp
@@ -1,27 +1,22 @@
 #include "StdAfx.h"
 #include "account.h"
-#include "resource.h"   //为了使用控件id
+#include "resource.h"  
 #include "workthread.h"
 
-
-
-
- 
 
 account::account()
 {
 	stock.code[0]=' ';
 	stock.quantity=0;
-    stock.price=0;
+	stock.price=0;
 	stock.N=0;
 	stock.Nnum=0;
 	stock.Hiprc=0;
 
 	max_profit=max_loss=profit=loss=0;
 	continuous_profit_temp=continuous_loss_temp=continuous_loss=continuous_profit=profit_times=loss_times=no_loss_win_times=0;
-	cash=initial_assets=total_assets=1000000;           //初始资金1000000
+	cash=initial_assets=total_assets=1000000;           //鍒濆璧勯噾1000000
 
-	fee_rat=1.01;    //百分之二的费用经过多次交易后对业绩的影响很大。
+	fee_rat=1.01;  
 	sign_buyprice=sign_sellprice=0;
-
 }
